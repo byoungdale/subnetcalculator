ea9bed1cbc9ea04dbb14e89aef79b7917d21aa37
byoungdale <brandon.youngdale@gmail.com>
2016-11-17 15:28:18 -0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Formsy from 'formsy-react';
+import logo from './logo.svg';
+import './App.css';
+import MyInput from './Input.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Subnet Calculator</h2>
+        </div>
+        <Formsy.Form onSubmit={this.DisplayInfo} className="custom-validation">
+          <MyInput name="ipaddress" title="IP Address" type="text" value="192.168.0.0/24"/>
+        </Formsy.Form>
+      </div>
+    );
+  }
+}
+
+export default App;
